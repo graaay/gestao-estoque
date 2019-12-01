9432ea6f793df788e40c06348353fe30cc1ef90f
graaay <abneralecs@gmail.com>
2019-12-01 18:34:40 -0300

    adicionado validates do movimento de estoque

--- app/models/movimento_estoque.rb
+++ app/models/movimento_estoque.rb
@@ -2,4 +2,7 @@ class MovimentoEstoque < ApplicationRecord
   belongs_to :pessoa
   belongs_to :operacao
   belongs_to :produto
+
+  validates :data, :quantidade, presence: true
+  validates :quantidade, numericality: {greater_than_or_equal_to => 1}
 end
